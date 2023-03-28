d73cd010fab785d493ea550ca45f876f538278a2
Bwa <40144023+beaesq@users.noreply.github.com>
2023-03-28 20:31:59 +0800

    Make initial file

---
+++ index.main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('stan loona');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXgubWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLDBCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vanMtdG9kby8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnc3RhbiBsb29uYScpOyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
